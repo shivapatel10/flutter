6e0cd48edd80ebcf37e0940b154b63fc9eadfea8
shivapatel10 <155878740+shivapatel10@users.noreply.github.com>
2025-03-13 12:47:44 +0530

    Update lab4.dart

--- lab4.dart
+++ lab4.dart
@@ -6,3 +6,16 @@ void main(List<String> arguments) {
 	lab4 lb = lab4();
 	print(lb.sname(x));
 }
+class lab4{
+	String sname(String color){
+		var choice = switch(color){
+			('red'|| 'Red'||'RED') => 'something',
+			('blue'|| 'Blue'||'BLUE') => 'another',
+			('black'||'Black'||'BLACK') => 'other',
+			('white'||'White'||'WHITE') => 'some',
+			_ => 'Notlisted',
+
+   };
+     return choice;
+}
+}
